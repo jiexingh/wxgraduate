7a11c336f130a2f8a7c9ca1005b0139c1014b0ab
jxh <2420499179@qq.com>
2019-02-26 23:52:06 +0800

    2019-02-26-23：50
    点菜

--- components/menu/index.js
+++ components/menu/index.js
@@ -11,13 +11,22 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    selected: false,
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    menuTap(event) {
+      let selected = !this.data.selected;
+      console.log(event);
+      this.setData({
+        selected: selected
+      })
+      this.triggerEvent('menuTap', {
+        selected: selected
+      });
+    }
   }
 })
